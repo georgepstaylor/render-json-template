91d25ee70a5f8c71531707e5d774613cff998768
Chris Ramacciotti <chris@rama.io>
2022-10-04 22:40:21 -0500

    Uses RUNNER_TEMP for root directory of output file

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 JSON_FILE_PATH=$1
 FIELD_VALUE_PAIRS=$2
 
-ROOT_DIR=${GITHUB_WORKSPACE:-./}
+ROOT_DIR=${RUNNER_TEMP:-./}
 
 OUTPUT_FILE_PATH="${ROOT_DIR}/$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '').json"
 
