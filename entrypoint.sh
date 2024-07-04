4d22b6d14391f2a5f97ca9132ff401d7ce2f742b
George Taylor <hey@george.dev>
2024-07-04 19:06:30 +0100

    updates + workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,7 @@
 JSON_FILE_PATH=$1
 FIELD_VALUE_PAIRS=$2
 
-ROOT_DIR=${RUNNER_TEMP:-./}
-
-OUTPUT_FILE_PATH="${ROOT_DIR}/$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '').json"
+OUTPUT_FILE_PATH="${RUNNER_TEMP:-./}/$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '').json"
 
 python -m render_json.render \
   --json-file-path "${JSON_FILE_PATH}" \
